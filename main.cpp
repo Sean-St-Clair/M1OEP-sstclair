114238f241e6e642919e4088e8e70f2d65e6093b
seans <sstclair@uvm.edu>
2023-02-02 23:05:26 -0800

    Switching to an int-based input loop with more options in the original prompt.

--- main.cpp
+++ main.cpp
@@ -5,21 +5,23 @@ using namespace std;
 // Testing some basic functionality without yet doing input validation
 // (Will do input validation once we are more sure of the structure of the program / input scheme)
 int main() {
-    cout << "Welcome to Dungeon builder! ";
+    cout << "Welcome to Dungeon Builder! ";
 
-    bool creating_dungeon = true;
-    string response;
+    string prompt = "What would you like to do?"
+                    "\n[1] - Create new room"
+                    "\n[2] - Copy existing room"
+                    "\n[3] - Link two rooms together"
+                    "\n[4] - Quit";
+    int response = 0;
     int num_rooms = 0;
     int room_width, room_height;
-    while (creating_dungeon) {
-        cout << "Would you like to add a room to your dungeon? [Y/N]" << endl;
+    while (response != 4) {
+        cout << prompt << endl;
         cin >> response;
         // Testing
         cout << response << endl;
-        if (response == "N") {
-            creating_dungeon = false;
+        if (response == 4)
             break;
-        }
         if (num_rooms == 0)
             cout << "Creating your first room!" << endl;
         cout << "Please enter an integer for the room width." << endl;
@@ -31,5 +33,8 @@ int main() {
         cout << "There are now " << num_rooms << " rooms." << endl;
         cout << "What would you like to do? (1 - create room, 2 - copy, 3 link two rooms)" << endl;
     }
+    cout << "Thank you for using Dungeon Builder! Your finished dungeon: " << endl;
+    // Print out an ascii visual of the dungeon
+    // Will limit the dimensions of the rooms so all prints out accordingly.
     return 0;
 }
