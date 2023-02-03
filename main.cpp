b045a37e32581605fb7dc7aec240346ffec2af7a
seans <sstclair@uvm.edu>
2023-02-02 23:23:18 -0800

    Testing new global functions for both splitting a string into a vector and printing the ascii dungeon

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+/*
+ * Takes a string and returns a vector of substrings split by the given delimiter
+ */
+vector<string> splitString(string input, char delimiter);
+
+/*
+ * Prints an ascii visual of the created dungeon.
+ */
+void printDungeon();
 
 // Testing some basic functionality without yet doing input validation
 // (Will do input validation once we are more sure of the structure of the program / input scheme)
@@ -13,8 +24,8 @@ int main() {
                     "\n[3] - Link two rooms together"
                     "\n[4] - Quit";
     int response = 0;
-    int num_rooms = 0;
-    int room_width, room_height;
+    int numRooms = 0;
+    int roomWidth, roomHeight;
     while (response != 4) {
         cout << prompt << endl;
         cin >> response;
@@ -22,19 +33,53 @@ int main() {
         cout << response << endl;
         if (response == 4)
             break;
-        if (num_rooms == 0)
+        if (numRooms == 0)
             cout << "Creating your first room!" << endl;
         cout << "Please enter an integer for the room width." << endl;
-        cin >> room_width;
+        cin >> roomWidth;
         cout << "Please enter an integer for the room height." << endl;
-        cin >> room_height;
-        cout << "Your room's dimensions are " << room_width << " x " << room_height << "." << endl;
-        num_rooms++;
-        cout << "There are now " << num_rooms << " rooms." << endl;
-        cout << "What would you like to do? (1 - create room, 2 - copy, 3 link two rooms)" << endl;
+        cin >> roomHeight;
+        cout << "Your room's dimensions are " << roomWidth << " x " << roomHeight << "." << endl;
+        numRooms++;
+        cout << "There are now " << numRooms << " rooms." << endl;
     }
     cout << "Thank you for using Dungeon Builder! Your finished dungeon: " << endl;
+    printDungeon();
     // Print out an ascii visual of the dungeon
     // Will limit the dimensions of the rooms so all prints out accordingly.
     return 0;
+}
+
+vector<string> splitString(string input, char delimiter) {
+    vector<string> splitVector;
+    int startIndex = 0;
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] == delimiter || i == input.size()) {
+            string subString = input.substr(startIndex, i);
+            splitVector.push_back(subString);
+            startIndex = i + 1;
+        }
+    }
+    return splitVector;
+}
+
+void printDungeon() {
+    int dungeonWidth = 5;
+    int dungeonHeight = 3;
+    string room = "----------"
+                  "\n|        |"
+                  "\n|        |"
+                  "\n|        |"
+                  "\n----------";
+    vector<string> split = splitString(room, '\n');
+    cout << split.size();
+    for (string test: split)
+        cout << test << endl;
+    // Print several rows of rooms on top of each other
+    for (int i = 0; i < dungeonHeight; i++) {
+        // Print several rooms next to each other in a single row
+        for (int e = 0; e < dungeonWidth; e++) {
+            // Split the room string by its newline characters in order to print several side-by-side.
+        }
+    }
 }
